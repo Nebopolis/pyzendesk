b96d529db207d0888f5166805e9fe062d3b3803f
Ben Evans <bevans@2476-bevans.local>
2014-06-05 20:52:12 -0500

    Vague start on seesion-handling code

--- pyzendesk/Session.py
+++ pyzendesk/Session.py
@@ -1,1 +1,25 @@
+import requests
+
 __author__ = 'bevans'
+
+subdomain = ''
+url = ''
+user = ''
+pwd = ''
+
+
+
+class Session:
+    
+    def __init__(subdomain,user,pwd):
+        
+
+def checkCreds(subdomain='z3nbe',user='bevans@zendesk.com',pwd='Oo87GVNSxuuN'):
+    url = 'https://{0}.zendesk.com/api/v2/tickets.json'.format(subdomain)
+    response = requests.get(url, auth=(user,pwd))
+    print(response.status_code)
+    return response
+    
+    
+    
+    
